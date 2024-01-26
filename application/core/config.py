928bc25ceb3027a3a828d87133fc396b75021a57
gglo0ol <124757163+gglo0ol@users.noreply.github.com>
2024-01-26 10:58:59 +0700

    add dockerfile

--- application/core/config.py
+++ application/core/config.py
@@ -8,9 +8,9 @@ POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 
-class Settings(BaseSettings):
 
-    connection_db: str = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}"
+class Settings(BaseSettings):
+    connection_db: str = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@database/{POSTGRES_DB}"  # localhost:5432
 
 
 settings = Settings()
