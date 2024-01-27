4b9c52c456e9869576a8b73acd332765ae91b4fc
gglo0ol <124757163+gglo0ol@users.noreply.github.com>
2024-01-27 18:58:14 +0700

    Add test_submenu_crud.py

--- tests/test_menu_crud.py
+++ tests/test_menu_crud.py
@@ -1,39 +1,64 @@
 from tests.conftest import client
 
 
-def test_get_all_menu_empty():
-    """get a empty list"""
+def create_menu(title: str, description: str):
+    response = client.post(
+        "/api/v1/menus/",
+        json={"title": title, "description": description},
+    )
+    return response
+
+
+def check_menu(menu_id: str):
+    response = client.get(f"/api/v1/menus/{menu_id}")
+    return response
+
+
+def get_all_menus():
     response = client.get("/api/v1/menus/")
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data == []
+    return response
+
+
+def update_menu(menu_id: str, new_title: str, new_description: str):
+    response = client.patch(
+        f"/api/v1/menus/{menu_id}",
+        json={
+            "title": new_title,
+            "description": new_description,
+        },
+    )
+    return response
+
+
+def delete_menu(menu_id: str):
+    response = client.delete(f"/api/v1/menus/{menu_id}")
+    return response
 
 
 def test_menu():
     """Test all menu function"""
 
     # get empty list
-    test_get_all_menu_empty()
+    get_empty_list = get_all_menus()
+    assert get_empty_list.status_code == 200, get_empty_list.text
+    assert get_empty_list.json() == []
 
-    # create a new menu and get json
-    create_menu_response = client.post(
-        "/api/v1/menus/",
-        json={"title": "My menu 1", "description": "My menu description 1"},
-    )
-    assert create_menu_response.status_code == 201, create_menu_response.text
-    data = create_menu_response.json()
+    # create a new menu
+    add_menu = create_menu(title="My menu 1", description="My menu description 1")
+    assert add_menu.status_code == 201, add_menu.text
+    data = add_menu.json()
     assert all(
         map(lambda item: item in data, ("id", "title", "description"))
     ), "error data"
     menu_id = data["id"]
 
     # Test list menu
-    all_menu_response = client.get("/api/v1/menus/")
+    all_menu_response = get_all_menus()
     assert all_menu_response.status_code == 200, all_menu_response.text
-    assert all_menu_response.json() == [data]
+    assert all_menu_response.json() != []
 
     # Test menu
-    get_menu_response = client.get(f"/api/v1/menus/{menu_id}")
+    get_menu_response = check_menu(menu_id=menu_id)
     assert get_menu_response.status_code == 200, get_menu_response.text
     assert all(
         map(
@@ -45,12 +70,8 @@ def test_menu():
     # Test update menu
     new_title, new_description = "My updated menu 1", "My updated menu description 1"
 
-    update_menu_response = client.patch(
-        f"/api/v1/menus/{menu_id}",
-        json={
-            "title": new_title,
-            "description": new_description,
-        },
+    update_menu_response = update_menu(
+        menu_id=menu_id, new_title=new_title, new_description=new_description
     )
     assert update_menu_response.status_code == 200, update_menu_response.text
     new_data = update_menu_response.json()
@@ -61,7 +82,7 @@ def test_menu():
     assert new_data["title"] == new_title and new_data["description"] == new_description
 
     # Check updated menu
-    get_updated_menu = client.get(f"/api/v1/menus/{menu_id}")
+    get_updated_menu = check_menu(menu_id=menu_id)
     assert get_updated_menu.status_code == 200, get_updated_menu.text
     updated_data = get_updated_menu.json()
     assert (
@@ -70,7 +91,7 @@ def test_menu():
     )
 
     # Test delete menu
-    delete_menu_response = client.delete(f"/api/v1/menus/{menu_id}")
+    delete_menu_response = delete_menu(menu_id=menu_id)
     assert delete_menu_response.status_code == 200, delete_menu_response.text
     assert delete_menu_response.json() == {
         "status": "true",
@@ -78,9 +99,11 @@ def test_menu():
     }
 
     # Test all menu empty list
-    test_get_all_menu_empty()
+    empty_list = get_all_menus()
+    assert empty_list.status_code == 200, get_empty_list.text
+    assert empty_list.json() == []
 
     # Test menu
-    get_menu_response = client.get(f"/api/v1/menus/{menu_id}")
+    get_menu_response = check_menu(menu_id=menu_id)
     assert get_menu_response.status_code == 404, get_menu_response.text
     assert get_menu_response.json() == {"detail": "menu not found"}
--- tests/test_submenu_crud.py
+++ tests/test_submenu_crud.py
@@ -1,26 +1,126 @@
-# from tests.conftest import client
-#
-#
-# def test_submenu_all_empty():
-#     get_submenu_all = client.get("")
-#
-#
-# def test_submenu():
-#     create_menu_response = client.post(
-#         "/api/v1/menus/",
-#         json={"title": "My menu 1", "description": "My menu description 1"},
-#     )
-#     assert create_menu_response.status_code == 201, create_menu_response.text
-#     data = create_menu_response.json()
-#     assert "id" in data
-#     menu_id = data["id"]
-#     all_menu_response = client.get("/api/v1/menus/")
-#     assert all_menu_response.json() == [data]
-#     create_submenu_response = client.post(
-#         f"/api/v1/menus/{menu_id}",
-#         json={"title": "My submenu 1", "description": "My submenu description 1"},
-#     )
-#     assert create_submenu_response.status_code == 201, create_menu_response.text
-#     data = create_menu_response.json()
-#     assert "id" in data
-#     submenu_id = data["id"]
+from tests.conftest import client
+from tests.test_menu_crud import create_menu, delete_menu, get_all_menus
+
+
+def create_submenu(menu_id: str, title: str, description: str):
+    response = client.post(
+        f"/api/v1/menus/{menu_id}/submenus",
+        json={"title": title, "description": description},
+    )
+    return response
+
+
+def check_submenu(menu_id: str, submenu_id):
+    response = client.get(f"/api/v1/menus/{menu_id}/submenus/{submenu_id}")
+    return response
+
+
+def check_all_submenu(menu_id: str):
+    response = client.get(f"/api/v1/menus/{menu_id}/submenus")
+    return response
+
+
+def update_submenu(menu_id: str, submenu_id: str, title: str, description: str):
+    response = client.patch(
+        f"/api/v1/menus/{menu_id}/submenus/{submenu_id}",
+        json={
+            "title": title,
+            "description": description,
+        },
+    )
+    return response
+
+
+def delete_submenu(menu_id: str, submenu_id: str):
+    response = client.delete(f"/api/v1/menus/{menu_id}/submenus/{submenu_id}")
+    return response
+
+
+def test_submenu():
+    # create menu
+    create_menu_response = create_menu(
+        title="My submenu 1", description="My submenu description 1"
+    )
+    assert create_menu_response.status_code == 201, create_menu_response.text
+    menu_data = create_menu_response.json()
+    assert "id" in menu_data
+    menu_id = menu_data["id"]
+
+    # Check list submenu (empty list)
+    all_submenu_response = check_all_submenu(menu_id=menu_id)
+    assert all_submenu_response.status_code == 200, all_submenu_response.text
+    assert all_submenu_response.json() == []
+
+    # Create submenu
+    submenu_title, submenu_description = "My submenu 1", "My submenu description 1"
+    create_submenu_response = create_submenu(
+        menu_id=menu_id, title=submenu_title, description=submenu_description
+    )
+    assert create_submenu_response.status_code == 201, create_menu_response.text
+    sub_data = create_submenu_response.json()
+    assert all(map(lambda item: item in sub_data, ("title", "description")))
+    assert "id" in sub_data
+    submenu_id = sub_data["id"]
+
+    # Test not empty list
+    check_submenu_response = check_all_submenu(menu_id=menu_id)
+    assert check_submenu_response.status_code == 200, check_submenu_response.text
+    assert check_submenu_response.json() != []
+
+    # Check submenu
+    get_submenu_response = check_submenu(menu_id=menu_id, submenu_id=submenu_id)
+    assert get_submenu_response.status_code == 200, get_submenu_response.text
+    data = get_submenu_response.json()
+    assert data["title"] == submenu_title and data["description"] == submenu_description
+
+    # Update submenu
+    new_title, new_description = (
+        "My updated submenu 1",
+        "My updated submenu description 1",
+    )
+    assert new_title != submenu_title and new_description != submenu_description
+    sub_update = update_submenu(
+        menu_id=menu_id,
+        submenu_id=submenu_id,
+        title=new_title,
+        description=new_description,
+    )
+    assert sub_update.status_code == 200, sub_update.text
+    update_data = sub_update.json()
+    assert (
+        update_data["title"] == new_title
+        and update_data["description"] == new_description
+    )
+
+    # Check submenu
+    check_sub_response = check_submenu(menu_id=menu_id, submenu_id=submenu_id)
+    assert check_sub_response.status_code == 200, check_sub_response.text
+    checked_data = check_sub_response.json()
+    assert (
+        update_data["id"] == checked_data["id"]
+        and checked_data["title"] == new_title
+        and checked_data["description"] == new_description
+    )
+
+    # Delete submenu
+    delete_submenu_response = delete_submenu(menu_id=menu_id, submenu_id=submenu_id)
+    assert delete_submenu_response.status_code == 200, delete_submenu_response.text
+
+    # Check submenu list (is empty)
+    check = check_all_submenu(menu_id=menu_id)
+    assert check.status_code == 200, check.text
+    assert check.json() == []
+
+    # Check submenu
+    sub = check_submenu(menu_id=menu_id, submenu_id=submenu_id)
+    assert sub.status_code == 404, sub.text
+    assert sub.json() == {"detail": "submenu not found"}
+
+    # Delete menu
+    menu_delete = delete_menu(menu_id=menu_id)
+    assert menu_delete.status_code == 200, menu_delete.text
+
+    # Check menu list (is empty)
+    menu_check = get_all_menus()
+    assert menu_check.status_code == 200, menu_check.text
+    assert menu_check.json() == []
